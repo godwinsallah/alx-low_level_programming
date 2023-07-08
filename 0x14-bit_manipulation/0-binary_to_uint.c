a86f9648eec18a65361fbdb7ab88a02c0a1e55a5
godwinsallah <godwinsallah16@gmail.com>
2023-07-08 00:03:58 +0000

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int c;
+	int c = 0;
 	unsigned int result = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	for (; b[c]; c++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[c] < '0' || b[c] > '1')
 			return (0);
-		result = 2 * result + (b[i] - '0');
+		result = 2 * result + (b[c] - '0');
 	}
 	return (result)
 }
