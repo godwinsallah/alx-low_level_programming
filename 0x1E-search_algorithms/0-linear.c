1d518be9a3807c758bc6d3d02439010eb38121f1
godwinsallah16 <godwinsallah16@gmail.com>
2024-02-10 21:18:36 +0000

    g

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,12 +1,14 @@
 #include "search_algos.h"
 
 /**
-  * linear_search - Searches for a value in an array.
+  * linear_search - Searches for a value in an array
+  *                 of integers using linear search.
   * @array: A pointer to the first element of the array to search.
   * @size: The number of elements in the array.
   * @value: The value to search for.
   *
-  * Return: 0 on success.
+  * Return: If the value is not present or the array is NULL, -1.
+  *         else, the first index where the value is located.
   *
   * Description: Prints a value every time it is compared in the array.
   */
